4127c105ce6fe740139d114611f6768059812cb7
Seetharaman GR <grsind19@gmail.com>
2021-12-31 07:14:10 +0530

    day 2 end

---
+++ Day2/Datatypes.java
@@ -0,0 +1,25 @@
+class Datatypes {
+  public static void main(String[] args){
+    String name = "6am club";
+    int time = 6;
+    Boolean is_daily = true;
+    float duration = 1.45f;
+    char section = 'A';
+
+    System.out.println(name); //this name
+    System.out.println(name.getClass().getName());
+
+    // System.out.println(time);
+    // System.out.println(time.getClass().getName());
+
+    System.out.println(is_daily);
+    System.out.println(is_daily.getClass().getName());
+
+    /* System.out.println(duration);
+    System.out.println(duration.getClass().getName());
+
+    System.out.println(section);
+    System.out.println(section.getClass().getName()); */
+
+  }
+}
